b38b93e9fd706e89ad5408c9cc6ec6773d4f498f
jsy <aa_nen@naver.com>
2025-02-03 22:01:21 +0900

    save

--- test/src/test7/JavaIf3.java
+++ test/src/test7/JavaIf3.java
@@ -19,46 +19,69 @@ public class JavaIf3 {
 		// 주차 몇시간 몇분 했는지 입력하여 주차요금이 얼마인지 출력해라
 		
 		Scanner sc = new Scanner(System.in);
-		int default_time = 30;
-		int default_time2 = 120;
-		int default_time3 = 240;
+//		int default_time = 30;
+//		int default_time2 = 120;
+//		int default_time3 = 240;
+//		int cost=1000;
+//		int cost2 = 1700;
+//		int cost3 = 2500;
+//		int cost4 = 10000;
+//		int pTime=0;
+//		int tenM=100;
+//		
+//		System.out.print("주차 시간(분) : ");
+//		pTime=sc.nextInt();
+//		
+//		if(0<pTime && pTime<=39)
+//			System.out.println("주차 요금 : "+cost);
+//		else if(119>=pTime){
+//			pTime=pTime-default_time;
+//			cost = (pTime/10)*tenM+cost;
+//			if(cost<=1700)
+//				System.out.println("주차 요금 : "+cost);
+//			else
+//				System.out.println("주차 요금 : "+1700);
+//		}
+//		else if(239>=pTime){
+//			pTime=pTime-default_time2;
+//			cost2 = (pTime/10)*tenM+cost2;
+//			if(cost<=2500)
+//				System.out.println("주차 요금 : "+cost2);
+//			else
+//				System.out.println("주차 요금 : "+2500);
+//		}
+//		else if(479>=pTime) {
+//			pTime=pTime-default_time3;
+//			cost3 = (pTime/10)*tenM+cost3;
+//			if(cost<=10000)
+//				System.out.println("주차 요금 : "+cost3);
+//			else
+//				System.out.println("주차 요금 : "+10000);
+//		}
+//		else
+//			System.out.println("주차 요금 : "+cost4);
+		int phour, pmin;
 		int cost=1000;
-		int cost2 = 1700;
-		int cost3 = 2500;
-		int cost4 = 10000;
-		int pTime=0;
-		int tenM=100;
+		int totalTime=0; // 총 주차 시간(분)- 기본 시간은 제외한 나머지
 		
-		System.out.print("주차 시간(분) : ");
-		pTime=sc.nextInt();
+		System.out.println("주차 시간(시간, 분) : ");
+		phour = sc.nextInt();
+		pmin = sc.nextInt();
 		
-		if(0<pTime && pTime<=39)
-			System.out.println("주차 요금 : "+cost);
-		else if(119>=pTime){
-			pTime=pTime-default_time;
-			cost = (pTime/10)*tenM+cost;
-			if(cost<=1700)
-				System.out.println("주차 요금 : "+cost);
-			else
-				System.out.println("주차 요금 : "+1700);
+		totalTime = phour*60+pmin;
+		if(phour >= 8) {
+			cost=10000;
+			totalTime=0;
+		} else if(phour >= 4) {
+			cost=2500;
+			totalTime -= 240;
+		} else if(phour>=2) {
+			cost=1700;
+			totalTime -= 120;
+		} else { // 2시간 미만일 경우
+			totalTime=totalTime < 30 ? 0:totalTime-30;
 		}
-		else if(239>=pTime){
-			pTime=pTime-default_time2;
-			cost2 = (pTime/10)*tenM+cost2;
-			if(cost<=2500)
-				System.out.println("주차 요금 : "+cost2);
-			else
-				System.out.println("주차 요금 : "+2500);
-		}
-		else if(479>=pTime) {
-			pTime=pTime-default_time3;
-			cost3 = (pTime/10)*tenM+cost3;
-			if(cost<=10000)
-				System.out.println("주차 요금 : "+cost3);
-			else
-				System.out.println("주차 요금 : "+10000);
-		}
-		else
-			System.out.println("주차 요금 : "+cost4);
+		int price = cost + (totalTime/10*100);
+		System.out.println("주차요금 : "+price+"원");
 	}
 }
