ebfeefabdcd165fd4fadccac1ddd0042ce726f79
jsy <aa_nen@naver.com>
2025-02-12 22:48:13 +0900

    save

--- test/src/test14/task.java
+++ test/src/test14/task.java
@@ -4,20 +4,17 @@ public class task {
 
 	public static void main(String[] args) {
 		int[] board = new int[25];
-		int num;
+		int num, n=0;
 		while(true) {
-			int i = 0;
 			num = (int)(Math.random()*50+1);
-			if(board[i]!=num) {
-				board[i] = num;
-				i++;
-			}
+			board[n] = num;
 			if(board[24]!=0)
 				break;
+			n++;
 		}
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<5; j++) {
-				System.out.print(board[i*5+j]+" ");
+				System.out.printf("%2d ", board[i*5+j]);
 			}
 			System.out.println();
 		}
