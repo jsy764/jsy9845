1eaa9ec2a9135c8c93c6bdfdd5a5f7843d42e440
jsy <aa_nen@naver.com>
2025-02-18 21:02:33 +0900

    save

---
+++ java_class1/src/study0218/ClassArray.java
@@ -0,0 +1,37 @@
+package study0218;
+
+public class ClassArray {
+
+	public static void main(String[] args) {
+//		Member mem = new Member();
+//		mem.name="김유신";
+//		mem.job="군인";
+//		mem.age=38;
+//		String name = new String();
+//		String[] names = new String[3];
+		
+//		Member[] members = new Member[5];
+//		// 5명의 정보를 저장만 하고 사용할 수 없음
+//		for(int i=0; i<members.length; i++)
+//			members[i]=new Member();
+//		
+//		members[0].job="경찰관";
+//		members[0].name="박문수";
+//		members[0].age=34;
+//		members[1].name="이순신";
+//		members[1].job="군인";
+//		members[1].age=31;
+//		members[2].name="문익점";
+//		members[2].age=45;
+//		members[2].job="산업스파이";
+//		for(int i=0; i<members.length; i++) {
+//			System.out.println(members[i].name);
+//			System.out.println(members[i].job);
+//			System.out.println(members[i].age);
+//		}
+		
+		Member info = new Member();
+		
+	}
+
+}
---
+++ java_class1/src/study0218/Member.java
@@ -0,0 +1,11 @@
+package study0218;
+
+public class Member {
+	String name; // 이름
+	int age; // 나이
+	String job; // 직업
+	
+	Member(){ // 생성자 메서드
+		System.out.println("나 생성자야");
+	}
+}
