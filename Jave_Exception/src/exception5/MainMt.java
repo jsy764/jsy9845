6d075076906dabd40efb66d550eb12e29d57f4d7
jsy <aa_nen@naver.com>
2025-03-05 13:31:43 +0900

    save

--- Jave_Exception/src/exception5/MainMt.java
+++ Jave_Exception/src/exception5/MainMt.java
@@ -1,5 +1,8 @@
 package exception5;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MainMt {
@@ -19,6 +22,32 @@ public class MainMt {
 //		} finally {
 //			System.out.println("총 금액 : " + total);
 //		} // 파일열기, 데이터베이스 연결에서는 반드시 파일을 닫고, 데이터베이스 연결 해제하는게 좋다.
+		
+		String fileName="c://test//data.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName)))
+		{
+			String line = br.readLine();
+			System.out.println(line);
+		}catch(Exception e) {
+			System.out.println("파일 열고 읽기 오류");
+			e.printStackTrace();
+		}
+//		BufferedReader read = null;
+//		try {
+//			read = new BufferedReader(new FileReader(fileName));
+//							String line = read.readLine();
+//							System.out.println(line);
+//		} catch(Exception e) {
+//			System.out.println("파일 열고 읽기 오류");
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				read.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
 		
 		
 		
---
+++ Jave_Exception/src/exception6/MainMt.java
@@ -0,0 +1,20 @@
+package exception6;
+
+public class MainMt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String keyword="찬양";
+		String content="나는 어제 친구와 술을 마셨다. 그친구의 이름은"+"찬약!!";
+		
+		try {
+			if(!content.contains(keyword)) {
+				throw new SearchException("검색결과 없다.", "111");
+			}
+		} catch(SearchException s) {
+			s.keywordMis();
+			s.printStackTrace();
+		}
+	}
+
+}
