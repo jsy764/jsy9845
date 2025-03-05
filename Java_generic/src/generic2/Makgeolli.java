7d311f038077c30441f0e4cd25d5f0d2ea2060ee
jsy <aa_nen@naver.com>
2025-03-05 21:30:29 +0900

    save

---
+++ Java_generic/src/generic2/Makgeolli.java
@@ -0,0 +1,14 @@
+package generic2;
+
+public class Makgeolli extends Alcohol{
+
+	public Makgeolli(String name, String cmp, int abv) {
+		super(name, cmp, abv);
+	}
+	
+	@Override
+	public String toString() {
+		return "소주 : "+cmp+", "+name+", 도수"+abv+"%";
+	}
+	
+}
