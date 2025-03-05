cf8261b4b85a9e423d77256170e2e308ea721c54
jsy <aa_nen@naver.com>
2025-03-05 11:04:01 +0900

    save

---
+++ Jave_Exception/src/exception5/MainMt.java
@@ -0,0 +1,30 @@
+package exception5;
+
+import java.util.Scanner;
+
+public class MainMt {
+
+	public static void main(String[] args) {
+		// 오류가 발생해도 오류가 없어도 무조건 실행
+//		Scanner sc = new Scanner(System.in);
+//		int money = 0;
+//		int total = 0;
+//		
+//		try {
+//			System.out.print("현금 : ");
+//			money = sc.nextInt();
+//			total = money*5;
+//		} catch(Exception e) {
+//			System.out.println("숫자만 입력하세요");
+//		} finally {
+//			System.out.println("총 금액 : " + total);
+//		} // 파일열기, 데이터베이스 연결에서는 반드시 파일을 닫고, 데이터베이스 연결 해제하는게 좋다.
+		
+		
+		
+		
+		
+		
+	}
+
+}
